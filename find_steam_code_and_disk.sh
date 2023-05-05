10bef07a102e3fc774c56f570aee68eb6e08da3a
matt <github.gitmatt@xoxy.net>
2023-05-06 01:04:44 +0200

    fix comments

--- find_steam_code_and_disk.sh
+++ find_steam_code_and_disk.sh
@@ -4,13 +4,13 @@
 find / -type f -name 'appmanifest*.acf' 2>/dev/null | while read file; do
     # check if the file path contains "steamapps"
     if [[ "$file" == *"steamapps"* ]]; then
-        # extract the text after "name" on the same line using grep and cut
+        # extract the text after "name" on the same line 
         name=$(grep -o 'name.*' "$file" | cut -d'"' -f3)
-        # extract the 4-8 figures number from the file name using a regular expression
+        # extract the 4-8 figures number from the file
         number=$(echo "$file" | grep -oE 'appmanifest_[0-9]{4,8}\.acf' | cut -d'_' -f2 | cut -d'.' -f1)
-        # extract the first directory name using dirname
+        # extract the first directory name 
         dir=$(dirname "$file" | cut -d'/' -f2)
-        # display the disk name, number, and name value as a table with a wider second column
+        # display the disk name, number, and name value as a table 
         printf "%-20s %-20s %s\n" "Disk: $dir" "Number: $number" "Name: $name"
     fi
 done
